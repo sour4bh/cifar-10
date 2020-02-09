68f7097b9ccb73880eb63047d91cf64aa9dcc845
Sourabh Sharma <lethaltranquillizer@gmail.com>
2020-02-09 14:56:44 +0530

    wideresnet

--- keras-cnn.py
+++ keras-cnn.py
@@ -147,4 +147,4 @@ print('Saved trained model at %s ' % model_path)
 # Score trained model.
 scores = model.evaluate(x_test, y_test, verbose=1)
 print('Test loss:', scores[0])
-print('Test accuracy:', scores[1])
+print('Test accuracy:', scores[1])  
