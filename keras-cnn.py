a288fdbf757c63c311f861d42ec9a684948cb710
Sourabh Sharma <lethaltranquillizer@gmail.com>
2020-02-09 13:17:35 +0530

    keras simple cnn

---
+++ keras-cnn.py
@@ -0,0 +1,150 @@
+# %%
+import pickle
+import numpy as np
+import os
+from functools import reduce
+
+import keras
+from keras.datasets import cifar10
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+# %%
+batches = os.listdir('dataset/')[1:]
+train_batches = batches[:-1]
+test_batch = batches[-1]
+train, labels = [], []
+for batch in train_batches:
+    with open(f'dataset/{batch}', 'rb') as f:
+        pdict = pickle.load(f, encoding='latin1')
+        train.append(pdict['data'].reshape((-1, 3, 32, 32)).transpose((0, 2, 3, 1)))
+        labels.append(np.array(pdict['labels']).reshape(-1, 1))
+
+with open(f'dataset/test_batch', 'rb') as f:
+    pdict = pickle.load(f, encoding='latin1')
+    test_batch = pdict['data'].reshape((-1, 3, 32, 32)).transpose((0,2,3,1))
+    test_labels = np.array(pdict['labels']).reshape(-1,1)
+
+
+def vstack(a, b): return np.vstack((a, b))
+
+
+train = reduce(vstack, train)
+labels = reduce(vstack, labels)
+mapping = dict(zip(range(10), ['airplane', 'automobile', 'bird',
+                               'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']))
+# %%
+num_classes = 10
+batch_size = 32
+epochs = 100
+
+# swtiches
+subtract_pixel_mean = 0
+data_augmentation = 1
+
+x_train, y_train = train, labels
+x_test, y_test = test_batch, test_labels
+
+# normalize pixel values
+x_train = x_train.astype('float32') / 255
+x_test = x_test.astype('float32') / 255
+
+# If subtract pixel mean is enabled
+if subtract_pixel_mean:
+    x_train_mean = np.mean(x_train, axis=0)
+    x_train -= x_train_mean
+    x_test -= x_train_mean
+
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+
+# %%
+model = Sequential()
+model.add(Conv2D(32, (3, 3), padding='same',
+                 input_shape=x_train.shape[1:]))
+model.add(Activation('relu'))
+model.add(Conv2D(32, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+model.add(Conv2D(64, (3, 3), padding='same'))
+model.add(Activation('relu'))
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Flatten())
+model.add(Dense(512))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes))
+model.add(Activation('softmax'))
+
+# initiate RMSprop optimizer
+opt = keras.optimizers.RMSprop(learning_rate=0.0001, decay=1e-6)
+
+# Let's train the model using RMSprop
+model.compile(loss='categorical_crossentropy',
+              optimizer=opt,
+              metrics=['accuracy'])
+
+
+if not data_augmentation:
+    print('Not using data augmentation.')
+    model.fit(x_train, y_train,
+              batch_size=batch_size,
+              epochs=epochs,
+              validation_data=(x_test, y_test),
+              shuffle=True)
+else:
+    datagen = ImageDataGenerator(
+        featurewise_center=False,  # set input mean to 0 over the dataset
+        samplewise_center=False,  # set each sample mean to 0
+        featurewise_std_normalization=False,  # divide inputs by std of the dataset
+        samplewise_std_normalization=False,  # divide each input by its std
+        zca_whitening=False,  # apply ZCA whitening
+        zca_epsilon=1e-06,  # epsilon for ZCA whitening
+        # randomly rotate images in the range (degrees, 0 to 180)
+        rotation_range=0,
+        # randomly shift images horizontally (fraction of total width)
+        width_shift_range=0.1,
+        # randomly shift images vertically (fraction of total height)
+        height_shift_range=0.1,
+        shear_range=0.,  # set range for random shear
+        zoom_range=0.,  # set range for random zoom
+        channel_shift_range=0.,  # set range for random channel shifts
+        # set mode for filling points outside the input boundaries
+        fill_mode='nearest',
+        cval=0.,  # value used for fill_mode = "constant"
+        horizontal_flip=True,  # randomly flip images
+        vertical_flip=False,  # randomly flip images
+        # set rescaling factor (applied before any other transformation)
+        rescale=None,
+        # set function that will be applied on each input
+        preprocessing_function=None,
+        # image data format, either "channels_first" or "channels_last"
+        data_format=None,
+        # fraction of images reserved for validation (strictly between 0 and 1)
+        validation_split=0.0)
+
+    datagen.fit(x_train)
+
+    model.fit_generator(datagen.flow(x_train, y_train,
+                                     batch_size=batch_size),
+                        epochs=epochs,
+                        validation_data=(x_test, y_test),
+                        workers=4)
+
+# %%
+if not os.path.isdir(save_dir):
+    os.makedirs(save_dir)
+model_path = os.path.join(save_dir, model_name)
+model.save(model_path)
+print('Saved trained model at %s ' % model_path)
+
+# Score trained model.
+scores = model.evaluate(x_test, y_test, verbose=1)
+print('Test loss:', scores[0])
+print('Test accuracy:', scores[1])
